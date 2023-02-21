e86ba154fb863c0c40003927054e6078df253085
mistralll <miyakei20011007@gmail.com>
2023-02-21 21:42:13 +0900

    iroiro

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-alert("check");
+const img = document.getElementById("img_szppi");
+img.animate(
+    [
+        {transform: 'rotateX(0deg)'},
+        {transform: 'rotateX(360deg)'}
+    ],
+    {
+        fill: 'backwards',
+        duration: 1000,
+        iterations: Infinity,
+    },
+)
